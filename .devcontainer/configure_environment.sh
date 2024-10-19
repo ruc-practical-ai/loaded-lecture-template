8d5e6b1c8fdb61809b8d8bf9ed9b8fa33718430c
Mauro J. Sanchirico III <sanchirico.mauro@gmail.com>
2024-10-19 14:30:50 +0000

    Add new devcontainer files

--- .devcontainer/configure_environment.sh
+++ .devcontainer/configure_environment.sh
@@ -1,3 +1,50 @@
+export DEBIAN_FRONTEND=noninteractive
+
+# ==========================
+# Basic package installation
+# ==========================
+
+echo "Installing basic packages..."
+
+apt-get -y update \
+&& apt-get install -yq curl vim git python3 python3-pip npm \
+&& apt-get clean
+
+if [ $? -eq 0 ]; then
+    echo "Basic packages installed!"
+else
+    echo "Failed to install basic packages."
+    exit 1
+fi
+
+echo "root ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+if grep -q "^root ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
+    echo "Enabled sudo for root!"
+else
+    echo "Failed to enable sudo for root."
+    exit 1
+fi
+
+echo "Changing ownership of all files in workspace to user group..."
+
+sudo chown -R developer:developer .
+
+if [ $? -eq 0 ]; then
+    echo "Success!"
+else
+    echo "Failed to change ownership."
+    exit 1
+fi
+
+echo "Linking Python to Python3..."
+
+ln -sf /usr/bin/python3 /usr/bin/python
+
+# ==========================
+# Extra package installation
+# ==========================
+
 echo "Installing http-server..."
 
 npm i -g http-server
@@ -11,8 +58,8 @@ fi
 
 echo "Installing texlive..."
 
-sudo apt-get -y update
-sudo apt-get -y install texlive
+apt-get -y update
+apt-get -y install texlive
 
 if [ $? -eq 0 ]; then
     echo "Texlive installed!"
@@ -21,7 +68,7 @@ else
     exit 1
 fi
 
-sudo apt-get -y install dvipng texlive-latex-extra texlive-fonts-recommended cm-super
+apt-get -y install dvipng texlive-latex-extra texlive-fonts-recommended cm-super
 
 if [ $? -eq 0 ]; then
     echo "Texlive extras and fonts installed!"
@@ -30,42 +77,9 @@ else
     exit 1
 fi
 
-echo "Installing Poetry..."
-
-pip install poetry
-
-if [ $? -eq 0 ]; then
-    echo "Poetry installed!"
-else
-    echo "Failed to install Poetry."
-    exit 1
-fi
-
-echo "Configuring Poetry virtual environments..."
-
-poetry config virtualenvs.in-project true
-
-if [ $? -eq 0 ]; then
-    echo "Virtual environments configured!"
-else
-    echo "Failed to configure virtual environments."
-    exit 1
-fi
-
-echo "Installing repository..."
-
-poetry install --with dev --no-root
-
-if [ $? -eq 0 ]; then
-    echo "Repository dependencies installed!"
-else
-    echo "Failed to install repository dependencies."
-    exit 1
-fi
-
 echo "Installing Git LFS..."
 
-sudo apt-get install git-lfs &&
+apt-get install git-lfs &&
 git lfs install
 
 if [ $? -eq 0 ]; then
@@ -75,24 +89,69 @@ else
     exit 1
 fi
 
+# =====================
+# Account configuration
+# =====================
+
+user_name="developer"
+group_name="developer"
+command_line_aliases_file="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/command_line.bash_aliases"
+git_aliases_file="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/git.bash_aliases"
+poetry_aliases_file="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/poetry.bash_aliases"
+
 echo "Getting convenient bash shortcuts..."
 
+developer_home="/home/$user_name"
+
 add_aliases() {
-    touch $HOME/.bash_profile
+    touch $developer_home/.bashrc
     file_name=$(basename "$1")
-    wget -P $HOME $1
-    echo "source ~/$file_name" >> $HOME/.bash_profile
+    curl -o "$developer_home/$file_name" $1
+    echo "source ~/$file_name" >> $developer_home/.bashrc
 }
 
-COMMAND_LINE_ALIASES_FILE="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/command_line.bash_aliases"
-GIT_ALIASES_FILE="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/git.bash_aliases"
-POETRY_ALIASES_FILE="https://raw.githubusercontent.com/mauro-j-sanchirico/personal-scripts/refs/heads/main/bash_aliases/poetry.bash_aliases"
+add_aliases $command_line_aliases_file
+add_aliases $git_aliases_file
+add_aliases $poetry_aliases_file
 
-add_aliases $COMMAND_LINE_ALIASES_FILE
-add_aliases $GIT_ALIASES_FILE
-add_aliases $POETRY_ALIASES_FILE
+echo 'source ~/.bashrc' >> $developer_home/.bash_profile
 
-echo 'source $HOME/.bash_profile' >> $HOME/.bashrc
+# ===================
+# Poetry installation
+# ===================
+
+poetry_dir=".local/bin"
+poetry_command="$developer_home/$poetry_dir/poetry"
+install_command="curl -sSL https://install.python-poetry.org | python3 -"
+
+echo "Installing Poetry..."
+
+sudo -u "$user_name" bash -c "$install_command"
+
+if "$poetry_command" --version &>/dev/null; then
+    echo "Poetry installed!"
+else
+    echo "Failed to install Poetry."
+fi
+
+echo "Adding Poetry to path..."
+
+echo "export PATH=\"$developer_home/$poetry_dir:\$PATH\"" >> $developer_home/.bashrc
+
+echo "Configuring Poetry virtual environments..."
+
+"$poetry_command" config virtualenvs.in-project true
+
+echo "Installing repository..."
+
+"$poetry_command" install --with dev --no-root
+
+if [ $? -eq 0 ]; then
+    echo "Repository dependencies installed!"
+else
+    echo "Failed to install repository dependencies."
+    exit 1
+fi
 
 echo "Success!"
 
--- .devcontainer/post_attach.sh
+++ .devcontainer/post_attach.sh
@@ -1,1 +1,31 @@
+echo "Configuring git to treat working directory as safe..."
 git config --global --add safe.directory $(pwd)
+
+if [ $? -eq 0 ]; then
+    echo "Successfully configured git!"
+else
+    echo "Failed to configure git to treat working directory as safe."
+fi
+
+echo ""
+echo "Sourcing .bash_profile..."
+source /home/developer/.bash_profile
+
+echo ""
+echo "Checking poetry version..."
+echo "Using poetry version:"
+poetry --version
+
+if [ $? -eq 0 ]; then
+    echo "Successfully installed poetry!"
+else
+    echo "Failed to install poetry."
+fi
+
+echo ""
+echo "Logged in as user:"
+whoami
+
+echo ""
+echo "Success - ready to develop!"
+echo ""
